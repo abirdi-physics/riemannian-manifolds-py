0bb94a9f73cae19327b7b4bdc223f890279acabe
Alexandre Birdi <alexandre.birdi@gmail.com>
2026-03-23 18:14:40 +0100

    Update: uploaded vectorized and agnostic sphere logic

--- sphere_frechet_mean.py
+++ sphere_frechet_mean.py
@@ -34,60 +34,44 @@ class Sphere(Manifold):
     
     def log(self, x, y):
         distance = self.dist(x, y)
-        if distance == 0:
-            return np.zeros(x.shape)
+        
+        if distance < 1e-15:
+            return np.zeros_like(x)
+        
         direction = self.proj(x,y)
-        pytha = 0
-        for i in range(len(direction)):
-            pytha += ((direction[i])**2)
-        normalised = direction / (pytha) ** 0.5
+        magnitude = np.linalg.norm(direction)
+        normalised = direction / magnitude
+        
         return normalised * distance
     
     def exp(self, x, v):
-        distance = 0
-        for i in range(len(v)):
-            distance += v[i] ** 2
-        magnitude = distance ** 0.5
-        if magnitude == 0:
+ 
+        magnitude = np.linalg.norm(v)
+        if magnitude < 1e-15:
             return x
         normalised_v = v / magnitude 
         return (np.cos(magnitude) * x) + (np.sin(magnitude) * normalised_v)
     
+
     def frechet_mean(self, data_points, max_iter = 100):
+        
         mu = data_points[0]
         
         for iterations in range(max_iter):
-            tangent_vectors = []
-            for i in range(len(data_points)):
-                tangent_vectors.append(self.log(mu, data_points[i]))
             
-            e1 = e2 = e3 = 0 #averaged unit vectors    
-            for j in range(len(tangent_vectors)):
-                e1 += tangent_vectors[j][0]
-                e2 += tangent_vectors[j][1]
-                e3 += tangent_vectors[j][2]
+            tangent_vectors = [ self.log(mu, p) for p in data_points]
             
-            averaged_vector = np.array([e1 / (len(data_points)), e2 /  (len(data_points)), e3 / (len(data_points))])
+            averaged_vector = np.mean(tangent_vectors, axis =0)
             
-            sum_sq= 0
-            for l in range(len(averaged_vector)):
-                sum_sq += averaged_vector[l] ** 2
-            magnitude = sum_sq ** 0.5
-            
-            if magnitude < 1e-9:
+            if np.linalg.norm(averaged_vector) < 1e-9:
                 break
+            
             mu = self.exp(mu, averaged_vector)
+        
         return mu
+        
+        
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         
         
